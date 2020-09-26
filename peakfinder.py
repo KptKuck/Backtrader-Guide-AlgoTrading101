e32781ca05deee18ed71271cfe425aaf51203ffb
Christian_x7 <xnet_dev@online.de>
2020-09-26 19:38:49 +0200

    add parameter dict for findpeaks

--- peakfinder.py
+++ peakfinder.py
@@ -3,33 +3,50 @@ import numpy as np
 import pandas as pd
 import advance_plotting as ADV
 from scipy.signal import find_peaks
+from collections import *
 
 bars = pd.read_csv('TSLA.csv')
 x = bars['Close']
 xNp = bars.to_numpy()
-#new_array = xNp.astype(type('float', (float,), {}))
-#yyy = np.invert(new_array)
-#xNpDD = np.expand_dims(xNp,axis=0)
-#xNpInv = np.linalg.inv(new_array)
+
+#fpPar = [namedtuple('Parameter', 'Name, Value')]
+fpParD ={
+  "distance": "10",
+  "prominence": "10",
+  "hight": "0"
+}
+fpPar = {}
+
+fpPar["height"] = None
+fpPar["threshold"] = None
+fpPar["distance"] = None
+fpPar["prominence"] = 30
+fpPar["width"] = None
+fpPar["wlen"] = None
+fpPar["rel_height"] = 0.5
+fpPar["plateau_size"] = None
 
 
-def findPeaks(x):
-    peaksH, temp1 = find_peaks(x[:,2])
-
-    peaksL, temp2 = find_peaks(-x[:,2])
-
-    return peaksH, peaksL
 
 
-peaksH, peaksL = findPeaks(xNp)
+
+def findPeaks(x,  par):
+    peaksH, propsH = find_peaks(x[:, 2], **par)
+
+    peaksL, propsL = find_peaks(-x[:, 2], **par)
+
+    return peaksH, peaksL, propsL, propsH
+
+
+peaksH, peaksL, propsL, propsH = findPeaks(xNp, fpPar)
 
 # ADV.plot_dataset(bars, 'TSLA')
 ADV.plot_bars_peaks(bars, peaksH, peaksL)
 
-#plt.plot(x)
+# plt.plot(x)
 
-#plt.plot(peaks, x[peaks], "x")
+# plt.plot(peaks, x[peaks], "x")
 
-#plt.plot(np.zeros_like(x), "--", color="gray")
+# plt.plot(np.zeros_like(x), "--", color="gray")
 
-#plt.show()
+# plt.show()
